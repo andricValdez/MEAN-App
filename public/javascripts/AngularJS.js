0d17ef76ac2a229dca9dfa95e571933326d6d273
Andric Valdez Valenzuela <andricvaldez@Andrics-MacBook-Pro-10.local>
2017-02-13 14:59:25 -0600

    15th commit

--- public/javascripts/AngularJS.js
+++ public/javascripts/AngularJS.js
@@ -50,7 +50,8 @@ app.controller('mainController', function ($scope, $http) {
 		vm.type_IniciarSeSion = 'logInLocal';
 		$http.post("/api/authenticate", {'password':vm.password_IniciarSeSion, 'email':vm.email_IniciarSeSion, 'type':vm.type_IniciarSeSion}).then(function(response) {
 			vm.token = response.data.token;
-       		vm.test = "Hola " + response.data.email + ". Este es tu token: " + response.data.token;
+       		// vm.test = "Hola " + response.data.email + ". Este es tu token: " + response.data.token;
+       		vm.test = response.data
        		console.log(response.data)
     		 
    		});
