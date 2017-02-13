58071017aa10412ef312e375694dd2a74531c55a
Andric Valdez Valenzuela <andricvaldez@Andrics-MacBook-Pro-10.local>
2017-02-13 15:23:11 -0600

    15th commit

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ apiRouter.post("/authenticate", function(req, res){
 
 	User.findOne({email: req.body.email}).select("email password").exec(function(errU, user){
 		// console.log(user._id); 
-		Session.findOne({user_id: user._id}).select("active").exec(function(errS, session){
+		Session.findOne({user_id: user._id}).select("active token").exec(function(errS, session){
 			console.log(session); 
 			if(!session){
 				console.log('session NO created'); 
@@ -56,8 +56,8 @@ apiRouter.post("/authenticate", function(req, res){
 
 				if (session.active == 'yes') {
 					//Sesion activada
-					return res.json(session);
-					 
+					return res.json(session, user);
+
 
 				}else{
 					saveSession(req, res, user, errU)
